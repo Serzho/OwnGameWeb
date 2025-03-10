ce8e978ca1653bec8a62967dbacac826f0788cf8
serzho <kudrsv.kudrsv@gmail.com>
2025-03-10 23:07:27 +0300

    + user table constraints

--- sql/main.sql
+++ sql/main.sql
@@ -4,3 +4,14 @@ CREATE TABLE "user"(
    email VARCHAR(255) NOT NULL,
    password CHAR(60) NOT NULL
 );
+
+ALTER TABLE "user"
+    ADD CONSTRAINT user_name_length CHECK (LENGTH(name) >= 3),
+    ADD CONSTRAINT user_email_unique UNIQUE (email),
+    ADD CONSTRAINT user_email_format CHECK (
+        email ~* '^[a-z0-9._+%-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+        ),
+    ADD CONSTRAINT user_password_length CHECK (LENGTH(password) = 60);
+
+CREATE UNIQUE INDEX user_email_unique_index
+    ON "user" (email);
